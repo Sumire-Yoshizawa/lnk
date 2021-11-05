cbe96e23f118b05c7239efe9ba6a123f86b61ebf
Yoshizawa <76186254+Sumire-Yoshizawa@users.noreply.github.com>
2021-11-05 15:05:20 +0700

    Update handleErrors.ts

--- src/api/middlewares/handleErrors.ts
+++ src/api/middlewares/handleErrors.ts
@@ -7,10 +7,12 @@ const handleErrors = (fn: NextApiHandler) => async (
 ) => {
   try {
     return await fn(req, res);
-  } catch (err) {
-    const statusCode = err.statusCode || 500;
+  } catch (error) {
+    const statusCode = error.statusCode || 500;
     const message = err.message || 'Oops, something went wrong!';
+if (error instanceof Error) {
     res.status(statusCode).json({ statusCode, message });
+}
   }
 };
 
